c0ac4d1d5765942a8bf75d89e83309dfe5275671
thekraalowais <thekra_alowais@hotmail.com>
2018-11-16 17:37:27 +0300

    img

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,10 +19,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     
-    # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
     # ActionController::Base.helpers.image_url("coin2.jpg") 
     # raise "image issue"
-    ActionController::Base.helpers.asset_path("default.png")
+    # ActionController::Base.helpers.asset_path("default.png")
 
       #  "/assets/" + [version_name, "default.png"].compact.join('_')
 
